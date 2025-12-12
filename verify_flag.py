72ab17790962627f7761d513e639c9d9d55288fa
Chijiro31 <145414624+Chijiro31@users.noreply.github.com>
2025-12-12 16:21:50 -0500

    Delete verify_flag.py

--- verify_flag.py
+++
@@ -1,5 +0,0 @@
-cts = [bytes.fromhex(l.strip()) for l in open("mtp.txt")]
-key = b"UNLP{we_4llLiv3inTheS4m3CountryCall3dCapitalism}"
-for i,c in enumerate(cts):
-    pt = bytes([ c[j] ^ key[j] for j in range(len(c)) ])
-    print(f"[{i}] {pt.decode('ascii','replace')}")
